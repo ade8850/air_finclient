878e325073fb8ca9d3109976c5d09295ddf73903
AdE <ade8850@gmail.com>
2024-09-29 18:44:05 +0200

    only year and month as integers

--- months.py
+++ months.py
@@ -3,16 +3,19 @@ from rich.table import Table
 import pandas as pd
 from finclient import containers
 
-def style_negative(val):
+def style_negative(val, column_name):
     if isinstance(val, bool):
-        return val and "[grey53]closed[/grey53]" or "[green]open[/green]"  
+        return val and "[grey53]closed[/grey53]" or "[green]open[/green]"
     elif isinstance(val, (int, float)):
-        formatted_val = round(val)  # f"{val:.2f}"  
-        if val < 0:
+        if column_name.lower() in ['year', 'month']:
+            formatted_val = int(val)
+        else:
+            formatted_val = f"{val:.2f}"
+        
+        if float(formatted_val) < 0:
             return f"[red]{formatted_val}[/red]"
         return str(formatted_val)
     return str(val)
-
 
 if __name__ == "__main__":
     container = containers.Container()
@@ -27,7 +30,7 @@ if __name__ == "__main__":
         table.add_column(column, justify="right")
 
     for _, row in df.iterrows():
-        table.add_row(*[style_negative(val) for val in row])
+        table.add_row(*[style_negative(val, col) for val, col in zip(row, df.columns)])
 
     console = Console()
     console.print(table)
